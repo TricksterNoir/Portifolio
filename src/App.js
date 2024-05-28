cbe4a4d165ca4844e9c523d7de33d20a60a3e74a
sylvio labriola <sylvioez@gmail.com>
2024-05-28 17:12:06 -0300

    First impressions and defined the colors of the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="divMaster">
+      <div className="App">     
+      <h1 className="masterTitle">I'm Sylvio Labriola</h1>
+      <h3 className="descriptionMasterTitle">and i'm a</h3>
+      <h2 className="developerWord">Software Developer</h2>
+      </div>
+
+      <div className="secondDiv">
+        <h1>Testing</h1>
+      </div>
     </div>
   );
 }
